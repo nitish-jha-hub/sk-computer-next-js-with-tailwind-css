482c4f8d3127dc577dd931452b5f5b64e581d94e
Nitish Jha <nitishkumar0914@gmail.com>
2022-05-17 01:33:11 +0530

    paytm payment gateway added "js cheakout"

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,10 +1,63 @@
 import React from 'react'
 import Link from 'next/link'
 import { AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai'
-
+import Head from 'next/head'
+import Script from 'next/script'
 const Checkout = ({ Cart, addToCart, removeFromCart, subTotal }) => {
+
+  const initiatepayment = async () => {
+
+    let oid = Math.floor(Math.random() * Date.now());
+
+    //get a transection tocken
+    const data = { Cart, subTotal, oid, email: "email" };
+
+    let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pretransection`, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    let txnRes = await a.json()
+    console.log(txnToken);
+    let txnToken = txnRes.txnToken
+
+    var config = {
+      "root": "",
+      "flow": "DEFAULT",
+      "data": {
+        "orderId": oid, /* update order id */
+        "token": txnToken, /* update token value */
+        "tokenType": "TXN_TOKEN",
+        "amount": subTotal /* update amount */
+      },
+      "handler": {
+        "notifyMerchant": function (eventName, data) {
+          console.log("notifyMerchant handler function called");
+          console.log("eventName => ", eventName);
+          console.log("data => ", data);
+        }
+      }
+    };
+
+
+    // initialze configuration using init method 
+    window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+      // after successfully updating configuration, invoke JS Checkout
+      window.Paytm.CheckoutJS.invoke();
+    }).catch(function onError(error) {
+      console.log("error => ", error);
+    });
+
+
+  }
+
   return (
+
     <div className=''>
+      <Head><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0" /></Head>
+      <Script type="application/javascript" crossorigin="anonymous" src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`} />
       <h2 className='text-center text-2xl font-serif font-semibold'>Checkout</h2>
       <div className='md:flex'>
         <div className='md:w-1/2 ml-2 mr-2'>
@@ -68,7 +121,7 @@ const Checkout = ({ Cart, addToCart, removeFromCart, subTotal }) => {
         </div>
       </div>
       <div className='items-center text-center'>
-        <button className="flex mx-auto mt-2 text-white bg-orange-600 border-0 py-2 px-8 focus:outline-none hover:bg-orange-700 rounded text-lg"> Pay ₹{subTotal}</button>
+        <button onClick={initiatepayment} className="flex mx-auto mt-2 text-white bg-orange-600 border-0 py-2 px-8 focus:outline-none hover:bg-orange-700 rounded text-lg"> Pay ₹{subTotal}</button>
       </div>
     </div >
   )
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -117,10 +117,10 @@ const Post = ({ addToCart }) => {
               </div>
             </div>
             <div className="flex flex-wrap">
-              <span className="title-font font-medium text-2xl text-gray-900">₹300.00</span>
+              <span className="title-font font-medium text-2xl text-gray-900">₹1.00</span>
               {/*product details */}
               <Link href="/checkout"><a className='flex ml-auto text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-700 rounded'><button onClick={() => addToCart(slug, 1, 300, 'Printer service', 'no', 'black')} className="">Buy Now</button></a></Link>
-              <button onClick={() => addToCart(slug, 1, 300, 'Printer service', 'no', 'black')} className="flex ml-auto text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-700 rounded">Add to Cart</button>
+              <button onClick={() => addToCart(slug, 1, 1, 'Printer service', 'no', 'black')} className="flex ml-auto text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-700 rounded">Add to Cart</button>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-600 ml-4">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
